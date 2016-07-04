3225d253f1da062b66beca732a59e697c509006c
Nik Butenko <nik@butenko.me>
2016-07-04 18:00:41 +1000

    Switch to @packetloop/section-iterator

--- src/Autowhatever.js
+++ src/Autowhatever.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import createSectionIterator from 'section-iterator';
+import createSectionIterator from '@packetloop/section-iterator';
 import themeable from 'react-themeable';
 
 function noop() {}
